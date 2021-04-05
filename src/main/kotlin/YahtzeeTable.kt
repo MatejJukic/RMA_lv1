398289e05787a2d6ebd739ecfaa93d4d08dba9fa
Matej-Jukic <matejjukiczuti@gmail.com>
2021-04-05 23:14:43 +0200

    added Poker, Yahtzee and Scale recognition

--- src/main/kotlin/YahtzeeTable.kt
+++ src/main/kotlin/YahtzeeTable.kt
@@ -59,22 +59,68 @@ data class YahtzeeTable (val playerNumber: Int, val numberOfFields: Int) {
 
     fun updateScore(keys: MutableList<String>, diceValues: List<String>): Boolean {
         var counter = 0
+
+        var counter1= 0
+        var counter2= 0
+        var counter3= 0
+        var counter4= 0
+        var counter5= 0
+        var counter6= 0
+
         if (keys.isEmpty()) return false
         val map: HashMap<String, Int> = playerResults[keys[0]] ?: return false
         when (keys[0]) {
             "Downward" -> {
                 when (keys[1]) {
-                    "Ones" -> {
-                        diceValues.forEach { if (it == "1") counter++ }
-                        map["Ones"] = counter
-                        map["Twos"] = -2
-                        return true
+                    "Poker" -> {
+                        diceValues.forEach {
+                            if (it=="1") counter1++
+                            if (it=="2") counter2++
+                            if (it=="3") counter3++
+                            if (it=="4") counter4++
+                            if (it=="5") counter5++
+                            if (it=="6") counter6++
+                        }
+                        if (counter1>3) map["Poker"] = 44
+                        if (counter2>3) map["Poker"] = 48
+                        if (counter3>3) map["Poker"] = 52
+                        if (counter4>3) map["Poker"] = 56
+                        if (counter5>3) map["Poker"] = 60
+                        if (counter6>3) map["Poker"] = 64
+
+                        counter1= 0
+                        counter2= 0
+                        counter3= 0
+                        counter4= 0
+                        counter5= 0
+                        counter6= 0
+
                     }
-                    "Twos" -> {
-                        diceValues.forEach { if (it == "2") counter++ }
-                        map["Twos"] = counter * 2
-                        map["Threes"] = -2
-                        return true
+
+                    "Yahtzee" -> {
+                        diceValues.forEach {
+                            if (it=="1") counter1++
+                            if (it=="2") counter2++
+                            if (it=="3") counter3++
+                            if (it=="4") counter4++
+                            if (it=="5") counter5++
+                            if (it=="6") counter6++
+                        }
+                        if (counter1>4) map["Poker"] = 55
+                        if (counter2>4) map["Poker"] = 60
+                        if (counter3>4) map["Poker"] = 65
+                        if (counter4>4) map["Poker"] = 70
+                        if (counter5>4) map["Poker"] = 75
+                        if (counter6>4) map["Poker"] = 80
+
+                        counter1= 0
+                        counter2= 0
+                        counter3= 0
+                        counter4= 0
+                        counter5= 0
+                        counter6= 0
+
+
                     }
 
                     "Scale" -> {
